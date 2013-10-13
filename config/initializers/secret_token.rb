7d45ec9c22cb7a5a992ef3d88e57bd14c81aa6e7
Sarah Allen <sarah@ultrasaurus.com>
2013-10-13 07:24:44 -0700

    vanilla rails app: --skip-active-record --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MongoPeople::Application.config.secret_key_base = 'c3dc5f8567fafd3df697006aba12aa556c0e552373d72319f9ba1f84cd1849a094e3a6143c4216ab776c62bf1d140077819b7d73934ec27baf8d6bf1c12ea2ef'
